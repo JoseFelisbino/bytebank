cd28cdd3751befdfa7ea4c7744858da26b026f6f
JoseFelisbino <lu31660@gmail.com>
2026-04-19 15:53:01 -0300

    feat: arrays e type alias

--- dist/js/typescript.js
+++ dist/js/typescript.js
@@ -1,6 +1,15 @@
 "use strict";
+// Tipos primitivos
 let valor = 3000;
 let nome = "";
 let isPago = false;
 let qualquer = "";
 qualquer = 22;
+// Arrays
+const lista = [];
+lista.push(13, 22);
+const novaTransacao = {
+    tipoTransacao: "",
+    data: new Date(),
+    valor: 0
+};
--- src/typescript.ts
+++ src/typescript.ts
@@ -1,5 +1,24 @@
+// Tipos primitivos
 let valor: number = 3000;
 let nome: string = "";
 let isPago: boolean = false;
 let qualquer: any = "";
 qualquer = 22;
+
+// Arrays
+const lista: number[] = [];
+lista.push(13, 22);
+
+// Tipos personalizados (Type Alias)
+type Transacao = {
+    tipoTransacao: string;
+    data: Date;
+    valor: number;
+}
+
+
+const novaTransacao: Transacao = {
+    tipoTransacao: "",
+    data: new Date(),
+    valor:0
+}
